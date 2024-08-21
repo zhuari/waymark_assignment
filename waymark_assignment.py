351941e248654f9c593a928a2db9f5583ce53b57
zhuari <49338223+zhuari@users.noreply.github.com>
2024-08-21 13:02:01 -0400

    Update waymark_assignment.py
    Added name

--- waymark_assignment.py
+++ waymark_assignment.py
@@ -1,17 +1,6 @@
-# ---
-# jupyter:
-#   jupytext:
-#     comment_magics: false
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.16.4
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
+'''
+Aric Zhu
+'''
 
 import boto3
 import pandas as pd
